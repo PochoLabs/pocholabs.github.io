ad7f473d74a7a72a0a7ac510f2ee0aed0228a408
Celso Mireles <celso@digitalstrategy.tips>
2016-02-10 21:11:11 -0700

    Add animations to text
    It may be too much, but for now, I’ll be annoyingly flashy.

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	new WOW().init();
+
+	$('.jumbo-heading').textillate({ in: { effect: 'zoomIn' } });
+
+	// $('.home-lead').on('mouseenter', function(){
+	// 	$(this).addClass('animated').addClass('pulse');
+	// });
+	// $('.home-lead').on('mouseleave', function(){
+	// 	$(this).delay(3000).removeClass('animated').removeClass('pulse');
+	// });
+});
