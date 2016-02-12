d2ab9661c667dda8d9497be9c37304e57fac2543
Celso Mireles <celso@digitalstrategy.tips>
2016-02-12 11:47:37 -0700

    Finalize content
    Add content to portfolio. Polish up the UI.

--- _harp/js/main.js
+++ _harp/js/main.js
@@ -3,6 +3,25 @@ $(document).ready(function(){
 
 	$('.jumbo-heading').textillate({ in: { effect: 'zoomIn' } });
 
+	$('.box-content').hover(function(){
+		$(this).find('i').addClass('animated').addClass('rubberBand');
+	}, function(){
+		$(this).find('i').removeClass('animated').removeClass('rubberBand');
+	})
+
+	$('.nav-link').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+
 	// $('.home-lead').on('mouseenter', function(){
 	// 	$(this).addClass('animated').addClass('pulse');
 	// });
