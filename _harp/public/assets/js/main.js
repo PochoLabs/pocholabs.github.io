c3e066e163bb6e27baefa1c4ce179c76da62b4fd
Celso Mireles <celso.mireles@gmail.com>
2016-12-28 13:36:23 -0700

    Build portfolio section

--- _harp/public/assets/js/main.js
+++ _harp/public/assets/js/main.js
@@ -22,8 +22,38 @@ hamburger.addEventListener('click', function() {
 });
 
 navLinks.forEach(link => link.addEventListener('click', function(e){
+  e.preventDefault();
   console.log('hide nav');
   collapsibleNav.classList.remove('display');
   collapsibleNav.setAttribute('data-visible', 'hidden');
   console.log(e);
+
+  var hash = e.srcElement.hash;
+  var target = document.querySelector(hash);
+
+  animate(document.scrollingElement || document.documentElement, "scrollTop", "", 0, target.offsetTop, 1000, true);
 }));
+
+
+function animate(elem, style, unit, from, to, time, prop) {
+    if (!elem) {
+        return;
+    }
+    var start = new Date().getTime(),
+        timer = setInterval(function () {
+            var step = Math.min(1, (new Date().getTime() - start) / time);
+            if (prop) {
+                elem[style] = (from + step * (to - from))+unit;
+            } else {
+                elem.style[style] = (from + step * (to - from))+unit;
+            }
+            if (step === 1) {
+                clearInterval(timer);
+            }
+        }, 25);
+    if (prop) {
+          elem[style] = from+unit;
+    } else {
+          elem.style[style] = from+unit;
+    }
+}
