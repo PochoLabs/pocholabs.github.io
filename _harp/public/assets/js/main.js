c61cda70522bbada1374a3f21feb0d97e8d143b0
Celso Mireles <celso.mireles@gmail.com>
2016-12-27 20:21:01 -0700

    Make mobil menu visible on click
    Needs a lot of work. Add animation, etc.

--- _harp/public/assets/js/main.js
+++ _harp/public/assets/js/main.js
@@ -1,1 +1,28 @@
 console.log('You have work to do...');
+
+// Nav menu
+
+var hamburger = document.querySelector('i.fa-bars');
+var collapsibleNav = document.querySelector('#collapsible-nav');
+var navLinks = document.querySelectorAll('a.nav-link');
+
+hamburger.addEventListener('click', function() {
+  var isVisible = this.dataset.visible === 'visible';
+  console.log('hamburger clicked');
+
+  if(isVisible) {
+    console.log('hide nav');
+    collapsibleNav.classList.remove('display');
+    this.setAttribute('data-visible', 'hidden');
+  } else {
+    console.log('make visible');
+    collapsibleNav.classList.add('display');
+    this.setAttribute('data-visible', 'visible');
+  }
+});
+
+navLinks.forEach(link => link.addEventListener('click', function(){
+  console.log('hide nav');
+  collapsibleNav.classList.remove('display');
+  collapsibleNav.setAttribute('data-visible', 'hidden');
+}));
